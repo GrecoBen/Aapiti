cd4e2386ae199b1893b0144109680d272d85b955
GrecoBen <greco.benjamin@orange.fr>
2023-11-08 12:06:16 +0100

    product_show

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,4 +25,15 @@ class ProductController extends AbstractController
             'allProduct' => $allProduct
         ]);
     }   
+
+     /**
+     * @Route("/product/{id}", name="app_product_show", requirements={"id"="\d+"})
+     * Homepage, display the selected comics
+     */
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
 }
